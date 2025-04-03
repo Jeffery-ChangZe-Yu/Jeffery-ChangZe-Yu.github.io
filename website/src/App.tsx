034e86040d743511d7bfb42e136a06b19d9ed511
Jeffery-ChangZe-Yu <57264662+Jeffery-ChangZe-Yu@users.noreply.github.com>
2025-04-03 16:52:14 -0400

    Adding navbar and placeholder name text

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Container, createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import Navbar from './layout/Navbar';
+
+
+const theme = createTheme({
+  //To-Do, Theme this up!!
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{
+        background: 'linear-gradient(60deg, purple, yellow)',
+        minHeight: '100vh'
+      }}>
+        <Navbar />
+        <Container maxWidth='xl'>
+          <Typography variant='h1'>Jeffery Yu</Typography>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
